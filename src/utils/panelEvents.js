b4a2b9d781af85e6fc7959066f7400c5b9526cc6
ManzDev <JoseRomanHernandez@gmail.com>
2019-06-26 22:20:49 +0100

    Field grass types

---
+++ src/utils/panelEvents.js
@@ -0,0 +1,15 @@
+const FIELD_TYPES = ['normal', 'classic', 'mosaic', 'dry', 'circle'];
+
+const dom = {
+  field: document.querySelector('.field')
+};
+
+let currentField = 0;
+
+addEventListener('keydown', function (event) {
+  if (event.key === 't') {
+    dom.field.classList.remove(FIELD_TYPES[currentField]);
+    currentField = (currentField + 1) % FIELD_TYPES.length;
+    dom.field.classList.add(FIELD_TYPES[currentField]);
+  }
+});
