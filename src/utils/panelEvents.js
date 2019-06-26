8628ba973fb3ce9d92d4c7e95945bfbbfaf07caf
ManzDev <JoseRomanHernandez@gmail.com>
2019-06-26 22:47:19 +0100

    Playable zone w/ players & equipation

--- src/utils/panelEvents.js
+++ src/utils/panelEvents.js
@@ -1,15 +1,22 @@
 const FIELD_TYPES = ['normal', 'classic', 'mosaic', 'dry', 'circle'];
+const TEAM_EQUIPATION = ['tenerife', 'barça', 'realmadrid', 'spain', 'atleti', 'pacman'];
 
 const dom = {
-  field: document.querySelector('.field')
+  field: document.querySelector('.field'),
+  playable: document.querySelector('.playable')
 };
 
 let currentField = 0;
+let currentEquipation = 0;
 
 addEventListener('keydown', function (event) {
   if (event.key === 't') {
     dom.field.classList.remove(FIELD_TYPES[currentField]);
     currentField = (currentField + 1) % FIELD_TYPES.length;
     dom.field.classList.add(FIELD_TYPES[currentField]);
+  } else if (event.key === 'e') {
+    dom.playable.classList.remove(TEAM_EQUIPATION[currentEquipation]);
+    currentEquipation = (currentEquipation + 1) % TEAM_EQUIPATION.length;
+    dom.playable.classList.add(TEAM_EQUIPATION[currentEquipation]);
   }
 });
