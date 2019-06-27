e841ff104810c3c426da829e3bcbd21d4b09d141
ManzDev <JoseRomanHernandez@gmail.com>
2019-06-27 13:50:19 +0100

    Refactor to PlayersZone

--- src/components/PlayersZone.js
+++ src/components/PlayersZone.js
@@ -1,8 +1,14 @@
 import '/components/TeamPlayer.js';
 
+const TEAM_EQUIPATION = ['tenerife', 'barça', 'realmadrid', 'spain', 'atleti', 'pacman'];
+const FORMATION_TYPES = ['initial', '1-2-6-2', '1-3-4-3', '1-5-2-3', '1-3-2-5', '1-5-1-4', '1-5-5-def'];
+
 class PlayersZone extends HTMLElement {
   constructor () {
     super();
+
+    this.currentEquipation = 0;
+    this.currentFormation = 1;
 
     this.classList.add('numbers', 'tenerife');
     this.innerHTML = `
@@ -17,6 +23,19 @@ class PlayersZone extends HTMLElement {
         <team-player number="9"></team-player>
         <team-player number="10"></team-player>
         <team-player number="11"></team-player>`;
+
+    addEventListener('keydown', this.keyListener.bind(this));
+  }
+
+  keyListener (event) {
+    if (event.key === 'e') {
+      this.classList.remove(TEAM_EQUIPATION[this.currentEquipation]);
+      this.currentEquipation = (this.currentEquipation + 1) % TEAM_EQUIPATION.length;
+      this.classList.add(TEAM_EQUIPATION[this.currentEquipation]);
+    } else if (event.key === 'f') {
+      this.currentFormation = (this.currentFormation + 1) % FORMATION_TYPES.length;
+      this.dataset.formation = FORMATION_TYPES[this.currentFormation];
+    }
   }
 }
 
--- src/utils/panelEvents.js
+++ src/utils/panelEvents.js
@@ -1,6 +1,4 @@
 const FIELD_TYPES = ['normal', 'classic', 'mosaic', 'dry', 'circle'];
-const TEAM_EQUIPATION = ['tenerife', 'barça', 'realmadrid', 'spain', 'atleti', 'pacman'];
-const FORMATION_TYPES = ['initial', '1-2-6-2', '1-3-4-3', '1-5-2-3', '1-3-2-5', '1-5-1-4', '1-5-5-def'];
 
 const dom = {
   field: document.querySelector('.field'),
@@ -8,20 +6,11 @@ const dom = {
 };
 
 let currentField = 0;
-let currentEquipation = 0;
-let currentFormation = 1;
 
 addEventListener('keydown', function (event) {
   if (event.key === 't') {
     dom.field.classList.remove(FIELD_TYPES[currentField]);
     currentField = (currentField + 1) % FIELD_TYPES.length;
     dom.field.classList.add(FIELD_TYPES[currentField]);
-  } else if (event.key === 'e') {
-    dom.playable.classList.remove(TEAM_EQUIPATION[currentEquipation]);
-    currentEquipation = (currentEquipation + 1) % TEAM_EQUIPATION.length;
-    dom.playable.classList.add(TEAM_EQUIPATION[currentEquipation]);
-  } else if (event.key === 'f') {
-    currentFormation = (currentFormation + 1) % FORMATION_TYPES.length;
-    dom.playable.dataset.formation = FORMATION_TYPES[currentFormation];
   }
 });
